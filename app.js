179dc2c8b5724f24362b6347667af4800e32d52d
Steph <blackman914037@gmail.com>
2020-03-09 23:17:30 +0800

    Initiate project

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express')
-const helpers = require('./_helpers');
-
+const helpers = require('./_helpers')
+const db = require('./models')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: 'false',
+    saveUninitialized: 'false'
+  })
+)
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-module.exports = app
+require('./routes')(app)

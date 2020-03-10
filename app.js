6ac429b95f2ce35927e8e294f9444ead88f32912
Steph <blackman914037@gmail.com>
2020-03-10 10:52:35 +0800

    Update res.locals

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
   res.locals.error_msg = req.flash('error_msg')
+  res.locals.user = helpers.getUser(req)
+  res.locals.isAuthenticated = helpers.ensureAuthenticated(req)
   next()
 })
 

38265021dcbaf74515dc6bb86766ba70cb4d8383
wendy03 <wendysu03@gmail.com>
2020-03-10 23:42:01 +0800

    Set Models

--- models/user.js
+++ models/user.js
@@ -1,8 +1,36 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  )
   User.associate = function(models) {
-  };
-  return User;
-};
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
+    User.hasMany(models.Like)
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    }) //User 的追蹤者
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    }) //User 追蹤中的 User
+  }
+  return User
+}
